ac0497ded7847e8e61170667b2aa50d524568ce8
csotot <66373682+csotot@users.noreply.github.com>
2023-06-23 23:43:54 -0500

    Test comit

--- pregunta.py
+++ pregunta.py
@@ -7,7 +7,8 @@ correctamente. Tenga en cuenta datos faltantes y duplicados.
 
 """
 import pandas as pd
-
+import re 
+from datetime import datetime
 
 def clean_data():
 
@@ -16,5 +17,32 @@ def clean_data():
     #
     # Inserte su código aquí
     #
+    #aca se deben eliminar los datos que falten y lo que falta
+    df.dropna (inplace=True)
+
+    df.sexo = df.sexo.str.lower 
+
+    df.tipo_de_emprendimiento = df.tipo_de_emprendimiento.str.lower()
+
+    df. idea_negocio = [str.lower(idea.replace("_", " "),replace("-", " ")) for idea in df.idea_negocio]
+
+    df.barrio = [stre.lower(barrio).replace("_", " ") . replace("-", " ") for barrio in df.barrio]
+
+    df.comuna_ciudadano = df.comuna_ciudadano.astype(int)
+
+    df.estrato = df.estrato.astype(int)
+
+    f["línea_credito"] = [str.lower(linea.strip().replace("-", " ").replace("_", " ").replace(". ", ".")) for linea in
+                           df["línea_credito"]]
+
+    df.fecha_de_beneficio = [datetime.strptime(date, "%d/%m/%Y") if bool(re.search(r"\d{1,2}/\d{2}/\d{4}", date))
+                             else datetime.strptime(date, "%Y/%m/%d")
+                             for date in df.fecha_de_beneficio]
+
+    df.monto_del_credito = [int(monto.replace("$ ", "").replace(".00", "").replace(",", "")) for monto in
+                            df.monto_del_credito]
+
+    df.drop_duplicates(inplace=True)
 
     return df
+
